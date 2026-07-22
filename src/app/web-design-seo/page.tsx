0b7c11c8c79c0ca86637237bed44bcf9fee28e8c
FingleHuang2715 <2678586420@qq.com>
2026-07-22 23:49:07 +0800

    Upload src/app/web-design-seo/page.tsx

---
+++ src/app/web-design-seo/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import GeoSeoOptimizationPage from "@/components/GeoSeoOptimization";
+
+export default function Page() {
+  return <GeoSeoOptimizationPage />;
+}
